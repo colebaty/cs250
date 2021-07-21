c48febbf93f270f2d58c095e70b10a1cb0abeccf
Cole <cole.baty@gmail.com>
2021-07-21 14:10:23 -0400

    changed INVALID to arbitrary assignment; fixed selecting A

--- Assignments/Senet/final_project_tbaty/senet_constants.h
+++ Assignments/Senet/final_project_tbaty/senet_constants.h
@@ -5,7 +5,7 @@ enum PlayerOnePieces : char {ONE = '1', TWO = '2', THREE = '3', FOUR = '4', FIVE
 enum PlayerTwoPieces : char {A = 'A', B = 'B', C = 'C', D = 'D', E = 'E'};
 enum SpecialSquares : int {TRAP = 26, LANDING = 14, FINISH = 29};
 enum SpecialPieces : char {TRAP_PIECE = 'X', LANDING_PIECE = 'O',
-                           EMPTY = ' ', INVALID = 0};
+                           EMPTY = ' ', INVALID};
 enum Move : char {ADVANCE, CAPTURE, NO_MOVE};
 
 enum Players: char {P1, P2, NONE};
